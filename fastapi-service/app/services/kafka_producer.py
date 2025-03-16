bece3b1492fbdc32b7ca46d4e8b5852ee75e1e31
yamtimor <yamtimor@gmail.com>
2025-03-16 18:20:47 +0200

    writing the producer

--- fastapi-service/app/services/kafka_producer.py
+++ fastapi-service/app/services/kafka_producer.py
@@ -5,3 +5,11 @@ from app.config.kafka_config import KAFKA_CONFIG, TOPIC_NAME
 # Initialize Kafka Producer
 producer = Producer(KAFKA_CONFIG)
 
+def send_to_kafka(key: str, value: dict):
+    try:
+        producer.produce(TOPIC_NAME, key=key, value=json.dumps(value))
+        producer.flush()
+        return {"status": "success", "message": "Event sent to Kafka"}
+    except Exception as e:
+        return {"status": "error", "message": f"Failed to send event to Kafka: {str(e)}"}
+    
